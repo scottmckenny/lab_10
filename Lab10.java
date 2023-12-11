641f9b3d88efb54841606ebe2edddcce46c4df34
Scott Mc Kenny <SCOTT.MCKENNY.2024@mumail.ie>
2023-12-11 09:21:06 +0000

    Removed the defining of "a ,b ,r" off there seperate lines, to avoid defining multiple times

--- Lab10.java
+++ Lab10.java
@@ -19,17 +19,16 @@ public class Lab10 {
             if (input.equals("q")) {
                 return;
             }
-
+            
+            double a, b ,r;
 
             if (input.equals("square")) {
-                double a;
                 System.out.println("Enter the length of side a: ");
                 a = Double.parseDouble(scan.nextLine());
                 System.out.println("The circumference of the square is: " + a * 4);
                 System.out.println("The area of the square is: " + a * a);
 
             } else if (input.equals("rectangle")) {
-                double a, b;
                 System.out.println("Enter the length of side a: ");
                 a = Double.parseDouble(scan.nextLine());
                 System.out.println("Enter the length of side b: ");
@@ -37,7 +36,6 @@ public class Lab10 {
                 System.out.println("The circumference of the rectangle is: " + (2 * a + 2 * b));
                 System.out.println("The area of the rectangle is: " + (a * b));
             } else if (input.equals("circle")) {
-                double r;
                 System.out.println("Enter the radius: ");
                 r = Double.parseDouble(scan.nextLine());
                 System.out.println("The circumference of the circle is: " + (Math.PI * r * 2));
