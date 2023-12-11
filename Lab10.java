5c700585e470aeae56bf6aac6fcdd4ddb3250b4b
Scott Mc Kenny <SCOTT.MCKENNY.2024@mumail.ie>
2023-12-11 09:49:55 +0000

    Added leap year function for febuary and made only possible dates be able to get printed

--- Lab10.java
+++ Lab10.java
@@ -49,101 +49,96 @@ public class Lab10 {
         int num = Integer.parseInt(scan.nextLine());
         System.out.println("Enter the current month: (1-12)");
         int num2 = Integer.parseInt(scan.nextLine());
+        
 
-        if (num == 1)
-            System.out.print("You selected 1st of ");
-        else if (num == 2)
-            System.out.print("You selected 2nd of ");
-        else if (num == 3)
-            System.out.print("You selected 3rd of ");
-        else if (num == 4)
-            System.out.print("You selected 4th of ");
-        else if (num == 5)
-            System.out.print("You selected 5th of ");
-        else if (num == 6)
-            System.out.print("You selected 6th of ");
-        else if (num == 7)
-            System.out.print("You selected 7th of ");
-        else if (num == 8)
-            System.out.print("You selected 8th of ");
-        else if (num == 9)
-            System.out.print("You selected 9th of ");
-        else if (num == 10)
-            System.out.print("You selected 10th of ");
-        else if (num == 11)
-            System.out.print("You selected 11th of ");
-        else if (num == 12)
-            System.out.print("You selected 12th of ");
-        else if (num == 13)
-            System.out.print("You selected 13th of ");
-        else if (num == 14)
-            System.out.print("You selected 14th of ");
-        else if (num == 15)
-            System.out.print("You selected 15th of ");
-        else if (num == 16)
-            System.out.print("You selected 16th of ");
-        else if (num == 17)
-            System.out.print("You selected 17th of ");
-        else if (num == 18)
-            System.out.print("You selected 18th of ");
-        else if (num == 19)
-            System.out.print("You selected 19th of ");
-        else if (num == 20)
-            System.out.print("You selected 20th of ");
-        else if (num == 21)
-            System.out.print("You selected 21st of ");
-        else if (num == 22)
-            System.out.print("You selected 22nd of ");
-        else if (num == 23)
-            System.out.print("You selected 23rd of ");
-        else if (num == 24)
-            System.out.print("You selected 24th of ");
-        else if (num == 25)
-            System.out.print("You selected 25th of ");
-        else if (num == 26)
-            System.out.print("You selected 26th of ");
-        else if (num == 27)
-            System.out.print("You selected 27th of ");
-        else if (num == 28)
-            System.out.print("You selected 28th of ");
-        else if (num == 29)
-            System.out.print("You selected 29th of ");
-        else if (num == 30)
-            System.out.print("You selected 30th of ");
-        else if (num == 31)
-            System.out.print("You selected 31st of ");
-        else {
-            System.out.println("Invalid day");
-        }
+    
+            if(num <= 28)
+            {
+                if(num2 == 2)
+                {
+                    System.out.print("You selected " + num2 + " of February");
+                }
+            }
+            if(num <= 29)
+            {
+                if(num2 == 2)
+                {
+                    System.out.println("Enter the current year");
+                    int num3 = Integer.parseInt(scan.nextLine());
+                    {
+                      if(num3 % 4 == 0)
+                        {
+                        System.out.print("You selected " + num2 + " of February");
+                        }  
+                    }
+                }
+            }
+            if(num <= 30)
+            {
 
-        if(num2 == 1) {
-            System.out.println("January");
-        } else if (num2 == 2) {
-            System.out.println("February");
-        } else if (num2 == 3) {
-            System.out.println("March");
-        } else if (num2 == 4) {
-            System.out.println("April");
-        } else if (num2 == 5) {
-            System.out.println("May");
-        } else if (num2 == 6) {
-            System.out.println("June");
-        } else if (num2 == 7) {
-            System.out.println("July");
-        } else if (num2 == 8) {
-            System.out.println("Augst");
-        } else if (num2 == 9) {
-            System.out.println("September");
-        } else if (num2 == 10) {
-            System.out.println("October");
-        } else if (num2 == 11) {
-            System.out.println("November");
-        } else if (num2 == 12) {
-            System.out.println("December");
-        } else {
-            System.out.println("Invalid month");
-        }
-    }
+                if(num2 == 4)
+                {
+                    System.out.print("You selected " + num2 + " of April");
+                }
+                if(num2 == 6)
+                {
+                    System.out.print("You selected " + num2 + " of June");
+                }
+                if(num2 == 9)
+                {
+                    System.out.print("You selected " + num2 + " of September");
+                }
+                if(num2 == 11)
+                {
+                    System.out.print("You selected " + num2 + " of November");
+                }
+            }
+            if(num <= 31)
+            {
+                if(num2 == 1)
+                {
+                    System.out.print("You selected " + num2 + " of January");
+                }
+                if(num2 == 3)
+                {
+                    System.out.print("You selected " + num2 + " of March");
+                }
+                if(num2 == 5)
+                {
+                    System.out.print("You selected " + num2 + " of May");
+                }
+                if(num2 == 7)
+                {
+                    System.out.print("You selected " + num2 + " of July");
+                }
+                if(num2 == 8)
+                {
+                    System.out.print("You selected " + num2 + " of August");
+                }
+                if(num2 == 10)
+                {
+                    System.out.print("You selected " + num2 + " of October");
+                }
+                if(num2 == 12)
+                {
+                    System.out.print("You selected " + num2 + " of December");
+                }
+                
+            }
+            if(num > 31 || num2 > 12)
+            {  
+                System.out.print("Invalid");
+            }
+                
+            }
+            
+            
+                
+            
+                
+
+        
+       
 
 public static void Q3() {
     System.out.println("Q3: Enter how many numbers you want to check for primality: ");
